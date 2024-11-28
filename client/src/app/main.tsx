7ad663247d2c69a2748c1a2d68f01d0b43d2eb20
Makaron77 <makasika47@gmail.com>
2024-11-28 10:53:22 +0300

    шаблон FSD

---
+++ client/src/app/main.tsx
@@ -0,0 +1,14 @@
+//* # Входная точка приложения
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './styles/index.css';
+import { BrowserRouter } from 'react-router-dom';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+	<BrowserRouter>
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>
+	</BrowserRouter>,
+);
